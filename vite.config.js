5dc2436f1f096443fb5af0ddb07b817b3abb59c3
KubeSlide <rajaabdul006@gmail.com>
2025-12-21 13:34:42 -0500

    Attempting to add Character selection page to build

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,19 @@
-export default{
-    base: process.env.NODE_ENV === 'production' ? '/MiniGameHub/' : ''
-}
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  base: "/MiniGameHub/",
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+
+        "character-selection": resolve(
+          __dirname,
+          "features/character-selection/character-selection.html"
+        ),
+      },
+    },
+  },
+});
