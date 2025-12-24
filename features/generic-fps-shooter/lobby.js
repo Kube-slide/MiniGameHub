6bc015e487d585cc5cd40e24901ef1bd45f2baf5
KubeSlide <rajaabdul006@gmail.com>
2025-12-23 20:56:49 -0500

    Implemented controls display on lobby

--- features/generic-fps-shooter/lobby.js
+++ features/generic-fps-shooter/lobby.js
@@ -1,19 +1,15 @@
-// Simple Modal Logic
 const modal = document.getElementById("controls-modal");
 const btn = document.getElementById("controls-btn");
 const close = document.getElementsByClassName("close-btn")[0];
 
-// Open
 btn.onclick = function () {
   modal.style.display = "flex";
 };
 
-// Close with X
 close.onclick = function () {
   modal.style.display = "none";
 };
 
-// Close by clicking outside
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
